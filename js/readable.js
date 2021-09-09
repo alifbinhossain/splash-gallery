89fe789a0cff6a676f087e7d1105adc5ed0664d2
alifbinhossain <you@example.com>
2021-09-09 10:58:27 +0600

    grid modified

--- js/readable.js
+++ js/readable.js
@@ -1,58 +1,67 @@
-// /* --------------------- REUSABLE VARIABLES & FUNCTIONS --------------------- */
-// const container = document.getElementById("container");
-// const photoGallery = document.getElementById("gallery");
-// const notifyText = document.getElementById("notify-text");
-// const spinner = `<div class="sk-chase mx-auto mt-4">
-//                  <div class="sk-chase-dot"></div>
-//                  <div class="sk-chase-dot"></div>
-//                  <div class="sk-chase-dot"></div>
-//                  <div class="sk-chase-dot"></div>
-//                  <div class="sk-chase-dot"></div>
-//                  <div class="sk-chase-dot"></div>
-//                  </div> `;
-// let searchResult;
+/* --------------------- REUSABLE VARIABLES & FUNCTIONS --------------------- */
+const container = document.getElementById("container");
+const photoGallery = document.getElementById("gallery");
+const notifyText = document.getElementById("notify-text");
+const spinner = `<div class="sk-chase mx-auto mt-4">
+                 <div class="sk-chase-dot"></div>
+                 <div class="sk-chase-dot"></div>
+                 <div class="sk-chase-dot"></div>
+                 <div class="sk-chase-dot"></div>
+                 <div class="sk-chase-dot"></div>
+                 <div class="sk-chase-dot"></div>
+                 </div> `;
+let searchResult;
 
-// /* ------------------------ LOAD DATA FUNCTIONALLITY ------------------------ */
-// const loadData = (Str) => {
-//   container.innerHTML = "";
-//   let searchText = Str;
-//   const inputField = document.getElementById("input-field");
-//   inputField.value != "" ? (searchText = inputField.value) : (searchText = Str);
-//   searchResult = searchText;
-//   inputField.value = "";
-//   const url = `https://api.unsplash.com/search/photos/?query=${searchText}&per_page=50&client_id=pl7Dj4uX_pWVpe-j4D7OQxNcOXnZgdHS5wvdBAA7ZAc
-//   `;
-//   notifyText.innerHTML = `Please wait for a moment.. <br>
-//                          ${spinner} `;
+/* ------------------------ LOAD DATA FUNCTIONALLITY ------------------------ */
+const loadData = (Str) => {
+  container.innerHTML = "";
+  let searchText = Str;
+  const inputField = document.getElementById("input-field");
+  inputField.value != "" ? (searchText = inputField.value) : (searchText = Str);
+  searchResult = searchText;
+  inputField.value = "";
+  const url = `https://api.unsplash.com/search/photos/?query=${searchText}&per_page=50&client_id=pl7Dj4uX_pWVpe-j4D7OQxNcOXnZgdHS5wvdBAA7ZAc
+  `;
+  notifyText.innerHTML = `Please wait for a moment.. <br>
+                         ${spinner} `;
 
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => displayImages(data.results.map((ele) => ele.urls.regular)));
-// };
-// loadData("kashmir");
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayImages(data.results));
+};
+loadData("kashmir");
 
-// /* ---------------------- DISPLAY IMAGE FUNCTIONALLITY ---------------------- */
-// const displayImages = (images) => {
-//   photoGallery.innerHTML = "";
+/* ---------------------- DISPLAY IMAGE FUNCTIONALLITY ---------------------- */
+const displayImages = (elements) => {
+  photoGallery.innerHTML = "";
 
-//   if (searchResult && images.length !== 0) {
-//     images.forEach((image) => {
-//       const photoPosition = Math.ceil(Math.random() * 4);
-//       const diffClass = `nor-${photoPosition}`;
+  if (searchResult && elements.length !== 0) {
+    elements.forEach((ele) => {
+      const newPhoto = document.createElement("div");
 
-//       const newPhoto = document.createElement("div");
-//       newPhoto.classList.add("photo", `${diffClass}`);
-//       newPhoto.innerHTML = `<img src="${image}" alt="" />`;
-
-//       photoGallery.appendChild(newPhoto);
-//       notifyText.innerHTML = `Result : <span class="text-warning">${searchResult}</span>`;
-//       container.appendChild(photoGallery);
-//     });
-//   } else {
-//     console.log(searchResult);
-//     searchResult === undefined
-//       ? (notifyText.innerText = `Please write specific name to get photos`)
-//       : (notifyText.innerHTML = `Sorry <span class="fw-bold text-danger">${searchResult}</span> is not found.`);
-//     container.innerHTML = ` <img class="d-block mx-auto w-50" src="images/not-found.png" alt="" />`;
-//   }
-// };
+      const width = ele.width;
+      const imgUrl = ele.urls.regular;
+      let diffClass;
+      if (width > 2500 && width < 4000) {
+        diffClass = "nor-4";
+      } else if (width > 5500) {
+        diffClass = "nor-2";
+      } else if (width < 2500) {
+        diffClass = "nor-3";
+      } else {
+        diffClass = "nor-1";
+      }
+      newPhoto.classList.add("photo", `${diffClass}`);
+      newPhoto.innerHTML = `<img src="${imgUrl}" alt="" />`;
+      photoGallery.appendChild(newPhoto);
+      notifyText.innerHTML = `Result : <span class="text-warning">${searchResult}</span>`;
+      container.appendChild(photoGallery);
+    });
+  } else {
+    console.log(searchResult);
+    searchResult === undefined
+      ? (notifyText.innerHTML = `<h3 class= "error-text">Please write specific name to get photos</h3>`)
+      : (notifyText.innerHTML = `<h3 class="error-text">Sorry <span class="fw-bold text-danger">${searchResult}</span> is not found.</h3>`);
+    container.innerHTML = ` <img class="d-block mx-auto w-50 full-width" src="images/not-found.png" alt="" />`;
+  }
+};
