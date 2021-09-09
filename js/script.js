8a7f58b138377d48ba477f75178c32d7979cdca2
alifbinhossain <you@example.com>
2021-09-09 15:40:49 +0600

    done

--- js/script.js
+++ js/script.js
@@ -1,138 +1,141 @@
-const _0x52aeab = _0x3564;
-(function (_0xdd613b, _0x2813f8) {
-  const _0x4e67b4 = _0x3564,
-    _0xe6efb3 = _0xdd613b();
+function _0xc604() {
+  const _0xbd49af = [
+    "141JCyibt",
+    "5bmIkdS",
+    "json",
+    "</span>",
+    "getElementById",
+    "then",
+    "<h3\x20class=\x22error-text\x22>Sorry\x20<span\x20class=\x22fw-bold\x20text-danger\x22>",
+    "13877793aiXnsD",
+    "</span>\x20is\x20not\x20found.</h3>",
+    "forEach",
+    "container",
+    "gallery",
+    "2369400AXPAUZ",
+    "536834KnpzVR",
+    "div",
+    "length",
+    "createElement",
+    "1876854gqJJXk",
+    "<h3\x20class=\x20\x22error-text\x22>Please\x20write\x20specific\x20name\x20to\x20get\x20photos</h3>",
+    "<img\x20src=\x22",
+    "Result\x20:\x20<span\x20class=\x22text-warning\x22>",
+    "39532mskfcj",
+    "regular",
+    "urls",
+    "nor-3",
+    "innerHTML",
+    "\x20<img\x20class=\x22d-block\x20mx-auto\x20w-50\x20full-width\x22\x20src=\x22images/not-found.png\x22\x20alt=\x22\x22\x20/>",
+    "log",
+    "\x22\x20alt=\x22\x22\x20/>",
+    "kashmir",
+    "Please\x20wait\x20for\x20a\x20moment..\x20<br>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+    "add",
+    "&per_page=50&client_id=pl7Dj4uX_pWVpe-j4D7OQxNcOXnZgdHS5wvdBAA7ZAc\x0a\x20\x20",
+    "2201216isxCCq",
+    "classList",
+    "value",
+    "5564244CaVOha",
+  ];
+  _0xc604 = function () {
+    return _0xbd49af;
+  };
+  return _0xc604();
+}
+const _0x50b3ad = _0x232c;
+(function (_0x2867b4, _0x55aef4) {
+  const _0x437e81 = _0x232c,
+    _0xed1900 = _0x2867b4();
   while (!![]) {
     try {
-      const _0x1c6ddc =
-        (parseInt(_0x4e67b4(0xc1)) / 0x1) * (-parseInt(_0x4e67b4(0xc2)) / 0x2) +
-        parseInt(_0x4e67b4(0xcd)) / 0x3 +
-        parseInt(_0x4e67b4(0xdc)) / 0x4 +
-        -parseInt(_0x4e67b4(0xe0)) / 0x5 +
-        (-parseInt(_0x4e67b4(0xca)) / 0x6) * (parseInt(_0x4e67b4(0xd2)) / 0x7) +
-        parseInt(_0x4e67b4(0xe7)) / 0x8 +
-        parseInt(_0x4e67b4(0xe6)) / 0x9;
-      if (_0x1c6ddc === _0x2813f8) break;
-      else _0xe6efb3["push"](_0xe6efb3["shift"]());
-    } catch (_0x3d1cf8) {
-      _0xe6efb3["push"](_0xe6efb3["shift"]());
+      const _0x3d6b03 =
+        -parseInt(_0x437e81(0x1f9)) / 0x1 +
+        (parseInt(_0x437e81(0x201)) / 0x2) *
+          (parseInt(_0x437e81(0x1ec)) / 0x3) +
+        -parseInt(_0x437e81(0x1f8)) / 0x4 +
+        (parseInt(_0x437e81(0x1ed)) / 0x5) *
+          (-parseInt(_0x437e81(0x1fd)) / 0x6) +
+        -parseInt(_0x437e81(0x1eb)) / 0x7 +
+        parseInt(_0x437e81(0x1e8)) / 0x8 +
+        parseInt(_0x437e81(0x1f3)) / 0x9;
+      if (_0x3d6b03 === _0x55aef4) break;
+      else _0xed1900["push"](_0xed1900["shift"]());
+    } catch (_0x3afe77) {
+      _0xed1900["push"](_0xed1900["shift"]());
     }
   }
-})(_0x5300, 0xdfef7);
-const container = document[_0x52aeab(0xcf)](_0x52aeab(0xc6)),
-  photoGallery = document[_0x52aeab(0xcf)](_0x52aeab(0xbe)),
-  notifyText = document[_0x52aeab(0xcf)](_0x52aeab(0xcc)),
-  spinner = _0x52aeab(0xd3);
+})(_0xc604, 0x7c53e);
+const container = document[_0x50b3ad(0x1f0)](_0x50b3ad(0x1f6)),
+  photoGallery = document[_0x50b3ad(0x1f0)](_0x50b3ad(0x1f7)),
+  notifyText = document[_0x50b3ad(0x1f0)]("notify-text"),
+  spinner =
+    "<div\x20class=\x22sk-chase\x20mx-auto\x20mt-4\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20";
 let searchResult;
-function _0x3564(_0x5f0fbd, _0x41e5ab) {
-  const _0x530021 = _0x5300();
+function _0x232c(_0x618bc7, _0x5559f7) {
+  const _0xc6046b = _0xc604();
   return (
-    (_0x3564 = function (_0x356461, _0x336153) {
-      _0x356461 = _0x356461 - 0xbc;
-      let _0x511e74 = _0x530021[_0x356461];
-      return _0x511e74;
+    (_0x232c = function (_0x232c3b, _0x52e3ef) {
+      _0x232c3b = _0x232c3b - 0x1e5;
+      let _0x368b14 = _0xc6046b[_0x232c3b];
+      return _0x368b14;
     }),
-    _0x3564(_0x5f0fbd, _0x41e5ab)
+    _0x232c(_0x618bc7, _0x5559f7)
   );
 }
-const loadData = (_0x3abe6b) => {
-  const _0x3d4cb8 = _0x52aeab;
-  container[_0x3d4cb8(0xce)] = "";
-  let _0x44f763 = _0x3abe6b;
-  const _0x1503a6 = document["getElementById"]("input-field");
-  _0x1503a6[_0x3d4cb8(0xdb)] != ""
-    ? (_0x44f763 = _0x1503a6[_0x3d4cb8(0xdb)])
-    : (_0x44f763 = _0x3abe6b),
-    (searchResult = _0x44f763),
-    (_0x1503a6["value"] = "");
-  const _0x1c65a8 = _0x3d4cb8(0xd0) + _0x44f763 + _0x3d4cb8(0xc0);
-  (notifyText[_0x3d4cb8(0xce)] = _0x3d4cb8(0xe3) + spinner + "\x20"),
-    fetch(_0x1c65a8)
-      [_0x3d4cb8(0xe5)]((_0x37a882) => _0x37a882[_0x3d4cb8(0xbd)]())
-      [_0x3d4cb8(0xe5)]((_0xda78e9) =>
-        displayImages(
-          _0xda78e9[_0x3d4cb8(0xdd)][_0x3d4cb8(0xdf)](
-            (_0x42041f) => _0x42041f[_0x3d4cb8(0xd4)][_0x3d4cb8(0xc5)]
-          )
-        )
-      );
+const loadData = (_0x539b26) => {
+  const _0x208d9a = _0x50b3ad;
+  container[_0x208d9a(0x205)] = "";
+  let _0x52da99 = _0x539b26;
+  const _0x29db3e = document["getElementById"]("input-field");
+  _0x29db3e[_0x208d9a(0x1ea)] != ""
+    ? (_0x52da99 = _0x29db3e[_0x208d9a(0x1ea)])
+    : (_0x52da99 = _0x539b26),
+    (searchResult = _0x52da99),
+    (_0x29db3e[_0x208d9a(0x1ea)] = "");
+  const _0x12a3f9 =
+    "https://api.unsplash.com/search/photos/?query=" +
+    _0x52da99 +
+    _0x208d9a(0x1e7);
+  (notifyText[_0x208d9a(0x205)] = _0x208d9a(0x1e5) + spinner + "\x20"),
+    fetch(_0x12a3f9)
+      [_0x208d9a(0x1f1)]((_0x12ab50) => _0x12ab50[_0x208d9a(0x1ee)]())
+      ["then"]((_0x203fdf) => displayImages(_0x203fdf["results"]));
 };
-loadData(_0x52aeab(0xc9));
-const displayImages = (_0x5be436) => {
-  const _0x16a1ae = _0x52aeab;
-  (photoGallery["innerHTML"] = ""),
-    searchResult && _0x5be436[_0x16a1ae(0xc3)] !== 0x0
-      ? _0x5be436[_0x16a1ae(0xd8)]((_0x1bcd00) => {
-          const _0x4069fc = _0x16a1ae,
-            _0x3eae49 = Math[_0x4069fc(0xd6)](Math[_0x4069fc(0xe1)]() * 0x4),
-            _0x346f71 = _0x4069fc(0xd7) + _0x3eae49,
-            _0x209fce = document[_0x4069fc(0xda)](_0x4069fc(0xbf));
-          _0x209fce[_0x4069fc(0xbc)]["add"](_0x4069fc(0xc8), "" + _0x346f71),
-            (_0x209fce[_0x4069fc(0xce)] =
-              _0x4069fc(0xd5) + _0x1bcd00 + _0x4069fc(0xd1)),
-            photoGallery["appendChild"](_0x209fce),
-            (notifyText[_0x4069fc(0xce)] =
-              _0x4069fc(0xd9) + searchResult + _0x4069fc(0xde)),
-            container[_0x4069fc(0xcb)](photoGallery);
+loadData(_0x50b3ad(0x209));
+const displayImages = (_0xf071c8) => {
+  const _0x324322 = _0x50b3ad;
+  (photoGallery[_0x324322(0x205)] = ""),
+    searchResult && _0xf071c8[_0x324322(0x1fb)] !== 0x0
+      ? _0xf071c8[_0x324322(0x1f5)]((_0x3bc43f) => {
+          const _0x346927 = _0x324322,
+            _0xad0a99 = document[_0x346927(0x1fc)](_0x346927(0x1fa)),
+            _0x5a9ad7 = _0x3bc43f["width"],
+            _0x23a164 = _0x3bc43f[_0x346927(0x203)][_0x346927(0x202)];
+          let _0x48cdb0;
+          if (_0x5a9ad7 > 0x9c4 && _0x5a9ad7 < 0xfa0) _0x48cdb0 = "nor-4";
+          else {
+            if (_0x5a9ad7 > 0x157c) _0x48cdb0 = "nor-2";
+            else
+              _0x5a9ad7 < 0x9c4
+                ? (_0x48cdb0 = _0x346927(0x204))
+                : (_0x48cdb0 = "nor-1");
+          }
+          _0xad0a99[_0x346927(0x1e9)][_0x346927(0x1e6)](
+            "photo",
+            "" + _0x48cdb0
+          ),
+            (_0xad0a99[_0x346927(0x205)] =
+              _0x346927(0x1ff) + _0x23a164 + _0x346927(0x208)),
+            photoGallery["appendChild"](_0xad0a99),
+            (notifyText[_0x346927(0x205)] =
+              _0x346927(0x200) + searchResult + _0x346927(0x1ef)),
+            container["appendChild"](photoGallery);
         })
-      : (console[_0x16a1ae(0xe4)](searchResult),
+      : (console[_0x324322(0x207)](searchResult),
         searchResult === undefined
-          ? (notifyText[_0x16a1ae(0xc7)] =
-              "Please\x20write\x20specific\x20name\x20to\x20get\x20photos")
-          : (notifyText[_0x16a1ae(0xce)] =
-              _0x16a1ae(0xc4) + searchResult + _0x16a1ae(0xe2)),
-        (container[_0x16a1ae(0xce)] =
-          "\x20<img\x20class=\x22d-block\x20mx-auto\x20w-50\x22\x20src=\x22images/not-found.png\x22\x20alt=\x22\x22\x20/>"));
+          ? (notifyText[_0x324322(0x205)] = _0x324322(0x1fe))
+          : (notifyText["innerHTML"] =
+              _0x324322(0x1f2) + searchResult + _0x324322(0x1f4)),
+        (container[_0x324322(0x205)] = _0x324322(0x206)));
 };
-function _0x5300() {
-  const _0xe09cfe = [
-    "div",
-    "&per_page=50&client_id=pl7Dj4uX_pWVpe-j4D7OQxNcOXnZgdHS5wvdBAA7ZAc\x0a\x20\x20",
-    "23AeovoI",
-    "103742wFsIyU",
-    "length",
-    "Sorry\x20<span\x20class=\x22fw-bold\x20text-danger\x22>",
-    "regular",
-    "container",
-    "innerText",
-    "photo",
-    "kashmir",
-    "10095018ryTccO",
-    "appendChild",
-    "notify-text",
-    "4762104VVwwMG",
-    "innerHTML",
-    "getElementById",
-    "https://api.unsplash.com/search/photos/?query=",
-    "\x22\x20alt=\x22\x22\x20/>",
-    "7zXEFBk",
-    "<div\x20class=\x22sk-chase\x20mx-auto\x20mt-4\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22sk-chase-dot\x22></div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x20",
-    "urls",
-    "<img\x20src=\x22",
-    "ceil",
-    "nor-",
-    "forEach",
-    "Result\x20:\x20<span\x20class=\x22text-warning\x22>",
-    "createElement",
-    "value",
-    "725832xbTYal",
-    "results",
-    "</span>",
-    "map",
-    "3077420RCIIuj",
-    "random",
-    "</span>\x20is\x20not\x20found.",
-    "Please\x20wait\x20for\x20a\x20moment..\x20<br>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
-    "log",
-    "then",
-    "14635377hhXjKK",
-    "8106240fZQXkJ",
-    "classList",
-    "json",
-    "gallery",
-  ];
-  _0x5300 = function () {
-    return _0xe09cfe;
-  };
-  return _0x5300();
-}
